868b2be998c41cdf12525dc924020411472ae841
sanjaybaskar <sanjay.baskar1828@gmail.com>
2022-11-12 23:11:19 +0530

    NAT gateway module added

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -19,3 +19,19 @@ module "vpc_module" {
     private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 
 }
+
+# create nat gateways 
+
+module "nat_module" {
+
+  source = "../modules/nat-gateway"
+  public_subnet_az1_id = module.vpc.public_subnet_az1_id
+  internet_gateway = module.vpc.internet_gateway
+  public_subnet_az2_id = module.vpc.public_subnet_az2_id
+  vpc_id = module.vpc.vpc_id
+  private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+  
+}
