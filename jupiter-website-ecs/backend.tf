2e57bf2bf02157eb24afa5eb2316e171aaa45cb8
sanjaybaskar <sanjay.baskar1828@gmail.com>
2022-11-16 20:32:53 +0530

    Revert "code chages done"
    This reverts commit f57152ebfba9f66409844e0613b4656bbab7837f.

--- jupiter-website-ecs/backend.tf
+++ jupiter-website-ecs/backend.tf
@@ -1,11 +1,8 @@
 # store the terraform state file in s3
 
 terraform {
-    backend "s3" {
-       bucket = "sk-terraform-data"
+    backend = "sk-terraform_data"
     key = "jupiter-website-ecs.tfstate"
     region = "ap-south-1"
-    #profile = "sanjay.baskar"
-    }
-   
+    profile = "sanjay.baskar"
 }
