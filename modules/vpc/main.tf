eb3740cbb11486ecc402de5820c0f95a6ebe8081
sanjaybaskar <sanjay.baskar1828@gmail.com>
2023-04-26 16:24:27 +0530

    updated vpc modules

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -64,7 +64,7 @@ resource "aws_route_table" "public_route_table" {
 
 #  associate public subnet az1 to "public route table"
 
-resource "aws_route_table_" "public_subnet_az1_rt_association" {
+resource "aws_route_table_association" "public_subnet_az1_rt_association" {
   subnet_id = aws_subnet.public_subnet_az1.id
   route_table_id = aws_route_table.public_route_table.id
 
@@ -72,7 +72,7 @@ resource "aws_route_table_" "public_subnet_az1_rt_association" {
 
 #  associate public subnet az2 to "public route table"
 
-resource "aws_route_table_" "public_subnet_az2_rt_association" {
+resource "aws_route_table_association" "public_subnet_az2_rt_association" {
   subnet_id = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_route_table.id
 
